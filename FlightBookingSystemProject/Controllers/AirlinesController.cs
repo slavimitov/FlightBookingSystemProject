321900d38742cf5323998a48f87121750f147695
SlaviMitov <forters4443@gmail.com>
2022-04-07 19:03:53 +0300

    Become airline

---
+++ FlightBookingSystemProject/Controllers/AirlinesController.cs
@@ -0,0 +1,52 @@
+﻿using FlightBookingSystemProject.Controllers;
+using FlightBookingSystemProject.Data;
+using FlightBookingSystemProject.Infastructure;
+using FlightBookingSystemProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class AirlinesController : Controller
+    {
+        private readonly FlightBookingDbContext data;
+
+        public AirlinesController(FlightBookingDbContext data)
+            => this.data = data;
+
+        [Authorize]
+        public IActionResult Become() => View();
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Become(BecomeAirlineFormModel airline)
+        {
+            var userId = this.User.Id();
+
+            var userIdAlreadyDealer = this.data
+                .Airlines
+                .Any(d => d.UserId == userId);
+
+            if (userIdAlreadyDealer)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(airline);
+            }
+
+            var airlineData = new Airline
+            {
+                Name = airline.Name,
+                UserId = userId
+            };
+
+            this.data.Airlines.Add(airlineData);
+            this.data.SaveChanges();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
---
+++ FlightBookingSystemProject/Infastructure/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace FlightBookingSystemProject.Infastructure
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+            => user.FindFirst(ClaimTypes.NameIdentifier).Value;
+    }
+}
