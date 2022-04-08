03f4a8d5dd085377f1135d0f5ac3cde7d6ae55e9
SlaviMitov <forters4443@gmail.com>
2022-04-08 20:46:08 +0300

    Added Search

--- FlightBookingSystemProject/Controllers/FlightsController.cs
+++ FlightBookingSystemProject/Controllers/FlightsController.cs
@@ -70,5 +70,49 @@ namespace FlightBookingSystemProject.Controllers
 
             return View(flights);
         }
+
+        public IActionResult Search(string origin, string destination, DateTime departureDate, DateTime returnDate, string travellers)
+        {
+
+            List<Flight> query = data.Flights.ToList();
+
+
+            if (!string.IsNullOrEmpty(origin))
+            {
+                query = query.Where(f => f.Origin == origin).ToList();
+            }
+            if (!string.IsNullOrEmpty(destination))
+            {
+                query = query.Where(f => f.Destination == destination).ToList();
+            }
+            if (departureDate != new DateTime(0001, 01, 01))
+            {
+                query = query.Where(f => f.DepartureDate == departureDate).ToList();
+            }
+            if (returnDate != new DateTime(0001, 01, 01))
+            {
+                query = query.Where(f => f.ReturnDate == returnDate).ToList();
+            }
+            if (!string.IsNullOrEmpty(travellers))
+            {
+                query = query.Where(f => data.Seats.Where(s => f.Id == s.FlightId).Where(s => s.IsBooked == false).Count() > int.Parse(travellers)).ToList();
+            }
+
+
+            var queryModel = new QueryModel();
+            queryModel.Flights = query
+                .Select(x => new AllFlightsViewModel
+                {
+                    Origin = x.Origin,
+                    Destination = x.Destination,
+                    DepartureDate = x.DepartureDate.ToString(),
+                    ReturnDate = x.ReturnDate.ToString(),
+                    Price = x.Price,
+                    DestinationImageUrl = x.DestinationImageUrl,
+                    FlightId = x.Id
+                }).ToList();
+
+            return View(queryModel);
+        }
     }
 }
---
+++ FlightBookingSystemProject/Models/QueryModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FlightBookingSystemProject.Models
+{
+    public class QueryModel
+    {
+        public IEnumerable<AllFlightsViewModel> Flights { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateOnly DepartureDate { get; set; }
+        public DateOnly ReturnDate { get; set; }
+        public string Travellers { get; set; }
+    }
+}
